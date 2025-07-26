eade941ca10d71ba36f21dd24ee7aa5c348a55f8
HemanthDL <hemanthdl2907@gmail.com>
2025-07-26 20:17:48 +0530

    Updated folders in accordance to hosting

--- Hosting/app.py
+++ Hosting/app.py
@@ -1,5 +1,4 @@
 from app.app import app  # Import the Flask app object from app/app.py
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(host='0.0.0.0', port=5000, debug=False)
